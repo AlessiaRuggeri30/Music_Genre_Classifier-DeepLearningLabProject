9638fbedb18d001afb6b9a013f1feb85fb8558a2
HeapHop30 <alessia.ruggeri.30@gmail.com>
2017-12-03 21:30:06 +0100

    Created momentary subset in json with features to work with

--- json_reader.py
+++ json_reader.py
@@ -8,12 +8,22 @@ file_directory = "trainingset/TRAAAAW128F429D538.json"
 json_data = open(file_directory).read()
 
 data = json.loads(json_data)
+to_store = {}
 
 for i in (data["datasets"].items()):
     item = i[0]
-    if data["datasets"][item]["alias"] == ['/analysis/segments_timbre']:
-        pprint(data["datasets"][item]["shape"]["dims"])
-        pprint(data["datasets"][item]["value"])
+    if data["datasets"][item]["alias"] == ['/analysis/songs']:
+        to_store["/analysis/songs"] = data["datasets"][item]
     if data["datasets"][item]["alias"] == ['/analysis/segments_start']:
-        pprint(data["datasets"][item]["shape"]["dims"])
-        pprint(data["datasets"][item]["value"])
+        to_store["/analysis/segments_start"] = data["datasets"][item]
+    if data["datasets"][item]["alias"] == ['/analysis/segments_timbre']:
+        to_store["/analysis/segments_timbre"] = data["datasets"][item]
+pprint(to_store)
+    # pprint(data["datasets"][item])
+        #pprint(data["datasets"][item]["shape"]["dims"])
+        #pprint(data["datasets"][item]["value"])
+    # if data["datasets"][item]["alias"] == ['/analysis/segments_start']:
+    #     pprint(data["datasets"][item]["shape"]["dims"])
+    #     pprint(data["datasets"][item]["value"])
+
+
